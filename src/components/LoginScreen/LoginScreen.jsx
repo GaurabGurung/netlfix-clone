a3ba47d56da2ccd214c7d0fb6b0c5383807d144d
Gaurab Gurung <gaurabgurung181@gmail.com>
2024-03-13 00:56:55 +1100

    added some styling on the login page

--- src/components/LoginScreen/LoginScreen.jsx
+++ src/components/LoginScreen/LoginScreen.jsx
@@ -1,22 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 import "./LoginScreen.scss";
 
 import Maxflix from "../../assets/maxflix.png";
 import maxflix_background from "../../assets/maxflix_background1.jpeg";
 
 const LoginScreen = () => {
+  const [signIn, setSignIn] = useState(false);
   return (
     <div
       className="login__container"
       style={{
-        backgroundImage: maxflix_background,
         backgroundImage: `url(${maxflix_background})`,
       }}
     >
       <div className="loginScreen__background">
         <img className="maxflix_log" src={Maxflix} alt="" />
-        <button className="signin_btn">Sign In</button>
+        <button className="signin_btn" onClick={() => setSignIn(true)}>
+          Sign In
+        </button>
         <div className="loginScreen_gradient" />
+        <div className="login_body">
+          <>
+            <h1>Unlimited films, TV programmes and more,</h1>
+            <h2>Watch anywhere. Cancel at any time.</h2>
+            <h3>
+              Ready to watch? Enter your email to create or restart your
+              membership
+            </h3>
+            <div className="loginScreen__input">
+              <form>
+                <input type="email" placeholder="Email Address" />
+                <button
+                  className="getStarted_btn"
+                  onClick={() => setSignIn(true)}
+                >
+                  GET STARTED
+                </button>
+              </form>
+            </div>
+          </>
+        </div>
       </div>
     </div>
   );
